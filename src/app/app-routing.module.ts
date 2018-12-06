4661816f75ec85760f1119149da09cf132645b86
Hongal <qnpfrajtls@gmail.com>
2018-12-06 23:50:40 +0900

    partial, if 작업

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IfComponent} from './if/if.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IfComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/if/if.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if',
+  templateUrl: './if.component.html',
+  styleUrls: ['./if.component.sass']
+})
+export class IfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
